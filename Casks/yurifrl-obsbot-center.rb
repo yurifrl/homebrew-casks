f45ba5bc2193d18afd5d59c00f1c593c6fd39756
Yuri <3260068+yurifrl@users.noreply.github.com>
2025-12-23 23:04:03 -0300

    Update to authorized CDN URL

--- Casks/yurifrl-obsbot-center.rb
+++ Casks/yurifrl-obsbot-center.rb
@@ -2,7 +2,7 @@ cask "yurifrl-obsbot-center" do
   version :latest
   sha256 :no_check
 
-  url "https://resource-cdn.obsbothk.com/download/obsbot-center/Obsbot_Center_OA_E_MacOS_Apple_2.0.13.28_release.dmg"
+  url "https://resource-cdn.obsbothk.com/202512241003/c87629316e32a62d7c2d7bac2dfc4b05/download/obsbot-center/Obsbot_Center_OA_E_MacOS_Apple_2.0.13.28_release.dmg"
   name "Obsbot Center"
   desc "Control center for Obsbot webcams"
   homepage "https://www.obsbot.com/"
